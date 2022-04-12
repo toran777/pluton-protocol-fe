2a8195c432e9162b78145098fb29ded1fc44d5fd
0x7183 <daniele.demenna@gmail.com>
2022-04-12 09:25:58 -0400

    Fixing phrases homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,8 @@ function HomePage() {
         <div className="container p-5">
             <div className="row col-12 mt-3 align-items-center justify-content-center">
                 <div className="col-4 mt-5">
-                    <h1><b>The best way to donate with crypto</b></h1>
-                    <h5>Use your crypto assets to help people for free.</h5>
+                    <h1><b>The best way to pay/donate with crypto</b></h1>
+                    <h5>Use your crypto assets to pay/help people for free.</h5>
                     <button className="btn-dark custom-btn-homepage mt-3">Donate</button>
                 </div>
                 <div className="col-7">
@@ -18,8 +18,8 @@ function HomePage() {
                 <div className="col-2" />
                 <div className="row col-8 mt-5 p-5 custom-card-homepage">
                     <div className="col-5 mt-5">
-                        <h2><b>Donate the APY of your crypto</b></h2>
-                        <h6>Deposit your assets and we'll redirect the APY to Companies or Individuals.</h6>
+                        <h2><b>Fund using the APY of your crypto</b></h2>
+                        <h6>Deposit your UST and redirect Anchor APY to anyone.</h6>
                         <button className="btn-dark custom-btn-homepage mt-3">Donate</button>
                     </div>
                     <div className="col-7">
@@ -38,7 +38,7 @@ function HomePage() {
                     <div className="col-5 mt-5">
                         <small>Example</small>
                         <h2><b>Plant a tree</b></h2>
-                        <h6>Deposit your assets here and we'll use the APY to plant trees using Treedom to make our own Pluton Forest.</h6>
+                        <h6>Deposit your assets here and we'll use Anchor APY to plant trees and make our own Pluton Forest on Treedom.</h6>
                         <button className="btn-dark custom-btn-homepage mt-3">Donate</button>
                     </div>
                 </div>
@@ -51,7 +51,7 @@ function HomePage() {
                     <div className="col-5 mt-5">
                         <small>Example</small>
                         <h2><b>Help the developers</b></h2>
-                        <h6>Help the developers pay their bills.</h6>
+                        <h6>We don't collect any fees from Pluton, if you like our work help us!</h6>
                         <button className="btn-dark custom-btn-homepage mt-3">Donate</button>
                     </div>
                     <div className="col-7">
@@ -72,10 +72,10 @@ function HomePage() {
                     <h6>Deposit your assets.</h6>
                     <hr className="mt-3" />
                     <h4><b>Step 2</b></h4>
-                    <h6>Choose the platform between Anchor Protocol and Mars Protocol.</h6>
+                    <h6>Insert the beneficiary address and an optional lock value.</h6>
                     <hr className="mt-3" />
                     <h4><b>Step 3</b></h4>
-                    <h6>Withdraw your assets when you need them back.</h6>
+                    <h6>Withdraw your assets when you need them back, or wait until the lock value is reached.</h6>
                 </div>
             </div>
         </div>
