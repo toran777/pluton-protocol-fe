900a47a8de7d3fafd2ac00a4c8cd31bc2390aee1
Robert Milos <r.milos@inno-tek.it>
2022-04-26 19:24:34 +0200

    card ui change

--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -1,44 +1,51 @@
-import { Fee, MsgExecuteContract, MsgSend } from '@terra-money/terra.js';
-import {CreateTxFailed, Timeout, TxFailed, TxUnspecifiedError, useConnectedWallet, UserDenied} from '@terra-money/wallet-provider';
-import React, { useCallback, useState } from 'react';
+import {MsgExecuteContract} from '@terra-money/terra.js';
+import {
+    CreateTxFailed,
+    Timeout,
+    TxFailed,
+    TxUnspecifiedError,
+    useConnectedWallet,
+    UserDenied
+} from '@terra-money/wallet-provider';
+import React from 'react';
 
 export function useWithdraw() {
-  const connectedWallet = useConnectedWallet();
-  const contractAddress = "terra1hd69jqjm8k5u6q53jm0kxpafgm95zr5faa2hgn"
+    const connectedWallet = useConnectedWallet();
+    const contractAddress = "terra1hd69jqjm8k5u6q53jm0kxpafgm95zr5faa2hgn"
 
-  const withdraw = (id) => {
-    
-    if (connectedWallet) {
-        connectedWallet
-        .post({
-            msgs: [
-                new MsgExecuteContract(
-                    connectedWallet.walletAddress,
-                    contractAddress,
-                    {"withdrawal": {"id": id}},
-                    0
-                )
-            ]
-        })
-        .catch((error) => {
-            if (error instanceof UserDenied) {
-            setTxError('User Denied');
-            } else if (error instanceof CreateTxFailed) {
-            setTxError('Create Tx Failed: ' + error.message);
-            } else if (error instanceof TxFailed) {
-            setTxError('Tx Failed: ' + error.message);
-            } else if (error instanceof Timeout) {
-            setTxError('Timeout');
-            } else if (error instanceof TxUnspecifiedError) {
-            setTxError('Unspecified Error: ' + error.message);
-            } else {
-            setTxError('Unknown Error: ' + (error instanceof Error ? error.message : String(error)),
-            );
+    const withdraw = (id) => {
+
+        if (connectedWallet) {
+            connectedWallet
+                .post({
+                    msgs: [
+                        new MsgExecuteContract(
+                            connectedWallet.walletAddress,
+                            contractAddress,
+                            {"withdrawal": {"id": id}},
+                            0
+                        )
+                    ]
+                })
+                .catch((error) => {
+                    if (error instanceof UserDenied) {
+                        setTxError('User Denied');
+                    } else if (error instanceof CreateTxFailed) {
+                        setTxError('Create Tx Failed: ' + error.message);
+                    } else if (error instanceof TxFailed) {
+                        setTxError('Tx Failed: ' + error.message);
+                    } else if (error instanceof Timeout) {
+                        setTxError('Timeout');
+                    } else if (error instanceof TxUnspecifiedError) {
+                        setTxError('Unspecified Error: ' + error.message);
+                    } else {
+                        setTxError('Unknown Error: ' + (error instanceof Error ? error.message : String(error)),
+                        );
+                    }
+                });
         }
-        });
     }
-}
-  return { withdraw }
+    return {withdraw}
 }
 
 export default useWithdraw
